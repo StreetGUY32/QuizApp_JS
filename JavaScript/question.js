032b8a563f619528528f8d346909f99fab26bd8e
StreetGUY32 <78868874+StreetGUY32@users.noreply.github.com>
2022-07-04 01:52:43 +0500

    Added few questions in questions.js

---
+++ JavaScript/question.js
@@ -0,0 +1,59 @@
+const questions = [
+    {
+        number:1,
+        question:'What does HTML Stands for?',
+        answer:'Hyper Text Markup Language',
+        options:[
+            'Home Tool MArkup Language',
+            'Hyperlinks and Text Markup Language',
+            'Hyper Text Markup Language',
+            'Hyper Tool Markup Language'
+            ]
+    },
+    {
+        number:2,
+        question:'What does CSS stand for?',
+        answer:'Cascading Style Sheet',
+        options:[
+            'Cascading Style Sheets',
+            'Computer Style Sheet',
+            'Colorful Style Sheets',
+            'Creative Style Sheet'
+        ]
+    },
+    {
+        number:3,
+        question:'What does PHP stand for?',
+        answer:'Hypertext Preprocessor',
+        options:[
+            'Preprocessed Hypertext page',
+            'Hypertext Preprocessor',
+            'Processor Hypertext Program',
+            'None of the above'
+        ]
+    },
+    {
+        number:4,
+        question:'Which of the below CSS is used to change color the text?',
+        answer:'color',
+        options:[
+            'background-color',
+            'color-background',
+            'color',
+            'None of the above'
+        ]
+    },
+    {
+        number:5,
+        question:'Which of the below is correct syntax to put a line over text in CSS ?',
+        answer:'text-decoration : overline',
+        options:[
+            'text-decoration : line',
+            'text-decoration : none',
+            'text-decoration : overline',
+            'text-decoration : underline'
+        ]
+    },
+
+]
+// You can add more questions here if you want - StreetGUY32
